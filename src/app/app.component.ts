f9abf6613e1658984a01008d080bb8d654a7d2de
aloksingh3112 <aloksingh3112@gmail.com>
2019-02-06 17:45:03 +0530

    added httpclient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-http-cache';
+  posts;
+
+  constructor(public httpClient:HttpClient){
+    const path: string ='https://jsonplaceholder.typicode.com/posts';
+    this.posts=this.httpClient.get<any>(path);
+
+
+  }
+
 }
